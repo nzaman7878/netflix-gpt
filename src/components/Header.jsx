938094618ef47f6b248d12755543ae5bdf71f2b3
nzaman7878 <nzaman7878>
2024-05-18 17:23:38 +0530

    implemented sign out feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,39 @@
-
+import { signOut } from "firebase/auth";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../utils/firebase";
 
 const Header = () => {
-  return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10"> 
-        <img 
-        className="w-44"
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"  
-        alt="netflix-logo"
-         />
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
 
-export default Header
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+
+  return (
+    <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+      <img
+        className="w-44"
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="logo"
+      />
+      {user && (
+        <div className="flex items-center p-2">
+          <img className="w-12 h-12 rounded-full" alt="usericon" src={user?.photoURL} />
+          <button onClick={handleSignOut} className="font-bold text-white ml-4">
+            Sign Out
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
