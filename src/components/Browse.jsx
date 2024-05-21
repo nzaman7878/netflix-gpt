396b0531c2011c19f021407e470d2ff56fb74c0c
nzaman7878 <nuruzzaman31032001@gmail.com>
2024-05-21 14:12:11 +0530

    added moviesSlice cmp

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,13 +1,18 @@
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import Header from "./Header";
 import { API_OPTIONS } from '../utils/constants';
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
+
   const getNowPlayingMovies = async () => {
     try {
-      const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS);
-      const json = await data.json();
-      console.log(json);
+      const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS);
+      const json = await response.json();
+      console.log(json.results);
+      dispatch(addNowPlayingMovies(json.results));
     } catch (error) {
       console.error('Error fetching now playing movies:', error);
     }
