22dc5a1211ec05cbab5620c321e08cf57e2c4dd2
nzaman7878 <nuruzzaman31032001@gmail.com>
2024-05-25 13:49:43 +0530

    updated secodary container page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,10 @@
-import { IMG_CDN_URL } from "../utils/constants"
+import { IMG_CDN_URL } from "../utils/constants";
 
-const MovieCard = ({posterPath}) => {
+const MovieCard = ({ posterPath }) => {
   return (
     <div className="w-48 pr-4">
-        <img src={IMG_CDN_URL + posterPath } 
-        alt="Movie Card" />
+      <img alt="Movie Card" src={IMG_CDN_URL + posterPath} />
     </div>
-  )
-}
-
-export default MovieCard
+  );
+};
+export default MovieCard;
--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -1,27 +1,24 @@
-import { useSelector } from "react-redux"
-import MovieList from "./MovieList"
-
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
 
 const SecondaryContainer = () => {
-const movies = useSelector((store)=> store.movies)
+  const movies = useSelector((store) => store.movies);
 
   return (
-    <div>
-      <MovieList title = {"Now Playing"} movies ={movies.nowPlayingMovies}/>
-
-
-{/* 
-    Movie List - Now playing 
-        -Movies card *n
-    Movie List - Trending
-    Movie List - Popular
-    Movie List - Horror 
-   
-    */}
-
-    </div>
-   
-  )
-}
-
-export default SecondaryContainer
+    movies.nowPlayingMovies && (
+      <div className="bg-black">
+        <div className="-mt-52 pl-12 relative z-20">
+          <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+          <MovieList
+            title={"Upcoming Movies"}
+            movies={movies.nowPlayingMovies}
+          />
+          <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
+        </div>
+      </div>
+    )
+  );
+};
+export default SecondaryContainer;
