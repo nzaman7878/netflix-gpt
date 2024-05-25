1535cb35eea8f918e52d49f7384bb96bc4e52137
nzaman7878 <nuruzzaman31032001@gmail.com>
2024-05-25 13:05:08 +0530

    created Movie Card cmp

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,10 @@
+import { IMG_CDN_URL } from "../utils/constants"
 
-
-const MovieCard = () => {
+const MovieCard = ({posterPath}) => {
   return (
-    <div>
-        
-
+    <div className="w-48 pr-4">
+        <img src={IMG_CDN_URL + posterPath } 
+        alt="Movie Card" />
     </div>
   )
 }
