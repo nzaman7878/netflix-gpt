de128d45a6dee21e1c90b75406f61084fdaab750
nzaman7878 <nuruzzaman31032001@gmail.com>
2024-05-25 13:29:52 +0530

    created movie list

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,24 +1,25 @@
-import MovieCard from "./MovieCard"
 
+import MovieCard from "./MovieCard";
 
-const MovieList = ({title , movies}) => {
-    console.log(movies);
+const MovieList = ({ title, movies }) => {
+  console.log(movies);
+
   return (
-    <div>
-     <div>
-        <h1>{title}</h1>
+    <div className="px-6">
+      <h1>{title}</h1>
+      <div className="flex overflow-x-scroll">
+        <div className="flex">
+          {movies && movies.length > 0 ? (
+            movies.map((movie) => (
+              <MovieCard key={movie.id} posterPath={movie.poster_path} />
+            ))
+          ) : (
+            <p>No movies available</p>
+          )}
         </div>
-        
-     <div>
-     <MovieCard />
-     <MovieCard />
-     <MovieCard />
-     <MovieCard />
-     </div>
-
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieList
+export default MovieList;
