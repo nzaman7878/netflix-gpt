16bbc79f7440191b099a5a192f33adf4d8d67b5b
nzaman7878 <nuruzzaman31032001@gmail.com>
2024-05-31 22:27:56 +0530

    update search results cmp

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,28 +1,29 @@
-
 import { useSelector } from "react-redux";
 
 const SearchResults = () => {
   const movieResults = useSelector((state) => state.search.movieResults);
 
   if (!movieResults || movieResults.length === 0) {
-    return <div>No results found.</div>;
+    return <div className="text-center mt-8 text-white">No results found.</div>;
   }
 
   return (
-    <div className="search-results grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 ">
-      {movieResults.map((movie) => (
-        <div key={movie.id} className="movie-result bg-white rounded-lg overflow-hidden shadow-lg">
-          <img
-            className="w-full h-auto"
-            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-            alt={movie.title}
-          />
-          <div className="p-4 ">
-            <h3 className="text-lg font-semibold mb-2">{movie.title}</h3>
-            <p className="text-gray-700 text-sm">{movie.overview}</p>
+    <div className="bg-gradient-to-t from-black min-h-screen p-4">
+      <div className="search-results grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
+        {movieResults.map((movie) => (
+          <div key={movie.id} className="movie-result bg-gray-800 rounded-lg overflow-hidden shadow-lg">
+            <img
+              className="w-full h-auto"
+              src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold mb-2 text-white">{movie.title}</h3>
+              <p className="text-gray-200 text-sm line-clamp-3">{movie.overview}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
